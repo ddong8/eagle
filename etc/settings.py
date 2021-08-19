c8365920451d001df3fc39f6b3e947b157c247ce
ddong8 <1413470138@qq.com>
2021-08-19 20:00:17 +0800

    rename container name.

--- etc/settings.py
+++ etc/settings.py
@@ -19,7 +19,7 @@ ACCESS_LOG = True
 WORKERS = workers
 
 DB_CFG = {
-    "host": "193.123.248.180",
+    "host": "eagle_db",
     "database": "ork",
     "user": "postgres",
     "password": "123456"
