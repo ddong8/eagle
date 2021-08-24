19d5f50a1f01e708f9e6900b172d5b75b49fd350
ddong8 <1413470138@qq.com>
2021-08-24 22:11:05 +0800

    add crontab task.

--- eagle/etc/settings.py
+++ eagle/etc/settings.py
@@ -6,6 +6,8 @@
 # Do have a faith in what you're doing.
 # Make your life a story worth telling.
 
+from celery.schedules import crontab
+
 HOST = '127.0.0.1'
 PORT = 9000
 
@@ -21,8 +23,8 @@ CELERY = {
     "timezone": 'Asia/Shanghai',
     "enable_utc": True,
     "worker_concurrency": 8,
-    "broker_url": "redis://www.ihasy.com/0",
-    "result_backend": "redis://www.ihasy.com/0",
+    "broker_url": "redis://eagle_redis/0",
+    "result_backend": "redis://eagle_redis/0",
     "imports": [
         "eagle.workers.stock.tasks"
     ],
@@ -34,12 +36,19 @@ CELERY = {
         "eagle.workers.*": {"queue": "eagle",
                             "exchange": "eagle",
                             "routing_key": "eagle"}
+    },
+    "beat_schedule": {
+        "add-every-workday-afternoon": {
+            "task": "eagle.workers.stock.tasks.add",
+            "schedule": crontab(hour=18, minute=0, day_of_week='mon,tue,wed,thu,fri'),
+            "args": [3, 6]
+        }
     }
 }
 
 CELERY_WORKER = {
     "callback": {
         "strict_client": True,
-        "allow_hosts": ["www.ihasy.com"]
+        "allow_hosts": ["eagle_redis"]
     }
 }
--- eagle/workers/stock/tasks.py
+++ eagle/workers/stock/tasks.py
@@ -1,13 +1,19 @@
+import datetime
+import io
 import json
 
+import pandas as pd
+import requests
 from eagle.common import async_helper, celery
 from eagle.workers.stock import callback
+from sqlalchemy import create_engine
 
 
 @celery.app.task
 def add(task_id, x, y):
     # task_id, x, y均为函数自定义参数，本次我们需要做回调演示，因此我们需要task_id异步任务id，以及加法的x和y
     result = x + y
+    get_today_top()
     # 这里还可以通知其他附加任务,当需要本次的一些计算结果来启动二次任务时使用
     # 接受参数：task调用函数路径 & 函数命名参数(dict)
     async_helper.send_task('eagle.workers.stock.tasks.other_task', kwargs={
@@ -28,3 +34,81 @@ def add(task_id, x, y):
 def other_task(task_id, result):
     # task_id, result均为函数自定义参数，本次我们需要做通知演示，因此我们需要task_id原始异步任务id，以及加法的result
     pass
+
+
+def get_yesterday(date_obj):
+    diff_day = datetime.timedelta(1)
+    date_obj -= diff_day
+    return date_obj
+
+
+def get_workday(date_obj):
+    if date_obj.weekday() + 1 in (1, 2, 3, 4, 5):
+        return date_obj
+    else:
+        date_obj = get_yesterday(date_obj)
+        return get_workday(date_obj)
+
+
+def write_to_table(df, table_name, if_exists='append'):
+    db_engine = create_engine(
+        'postgresql+psycopg2://postgres:123456@193.123.248.180:5432/ork')  # 初始化引擎
+    string_data_io = io.StringIO()
+    df.to_csv(string_data_io, sep='|', index=False)
+    pd_sql_engine = pd.io.sql.pandasSQL_builder(db_engine)
+    table = pd.io.sql.SQLTable(table_name, pd_sql_engine, frame=df,
+                               index=False, if_exists=if_exists)
+    table.create()
+    string_data_io.seek(0)
+    string_data_io.readline()  # remove header
+    with db_engine.connect() as connection:
+        with connection.connection.cursor() as cursor:
+            copy_cmd = "COPY {} FROM STDIN HEADER DELIMITER '|' CSV".format(
+                table_name)
+            cursor.copy_expert(copy_cmd, string_data_io)
+        connection.connection.commit()
+
+
+def get_top_list(date_obj):
+    df = raw_top_list(date_obj)
+    write_to_table(df, 'stock', if_exists='append')
+    return df
+
+
+def raw_top_list(date_str):
+    LHB_URL = 'http://data.eastmoney.com/DataCenter_V3/stock2016/TradeDetail/pagesize=200,page=1,sortRule=-1,sortType=,startDate={startDate},endDate={endDate},gpfw=0,js=vardata_tab_1.html'.format(
+        startDate=date_str, endDate=date_str)
+    req = requests.get(url=LHB_URL)
+    resp_text = req.text
+    resp_json = json.loads(resp_text.replace('vardata_tab_1=', ''))
+    resp_data = resp_json.get('data', [])
+    df = pd.DataFrame(resp_data, columns=[
+                      'SCode', 'SName', 'Chgradio', 'ZeMoney', 'Bmoney', 'Smoney', 'Ctypedes', 'Turnover'])
+    df.columns = ['code', 'name', 'pchange',
+                  'amount', 'buy', 'sell', 'reason', 'Turnover']
+    df = df.fillna(0)
+    df = df.replace('', 0)
+    df['id'] = range(len(df))
+    df['buy'] = df['buy'].astype(float)
+    df['sell'] = df['sell'].astype(float)
+    df['amount'] = df['amount'].astype(float)
+    df['Turnover'] = df['Turnover'].astype(float)
+    df['bratio'] = df['buy'] / df['Turnover']
+    df['sratio'] = df['sell'] / df['Turnover']
+    df['bratio'] = df['bratio'].map(lambda x: '%.2f' % x)
+    df['sratio'] = df['sratio'].map(lambda x: '%.2f' % x)
+    df['date'] = date_str
+    for col in ['amount', 'buy', 'sell']:
+        df[col] = df[col].astype(float)
+        df[col] = df[col] / 10000
+        df[col] = df[col].map(lambda x: '%.2f' % x)
+    df = df.drop('Turnover', axis=1)
+    return df
+
+
+def get_today_top():
+    today = datetime.date.today()
+    diff_day = datetime.timedelta(1)
+    dest_day = today - 0 * diff_day
+    dest_day_str = dest_day.isoformat()
+    get_top_list(dest_day_str)
