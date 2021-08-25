ffa069471a55396b13fafac4c6fe4f1eabfdaf6d
ddong8 <1413470138@qq.com>
2021-08-25 19:48:04 +0800

    update celery worker config.

--- celery_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python -m celery -A celery_worker worker -l info -c 10 -Q eagle
-python -m celery -A celery_worker beat -l info -c 10
-python -m celery -A celery_worker flower --address=0.0.0.0 --port=5555
