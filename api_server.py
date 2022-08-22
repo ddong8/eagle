5151505a617e1af8a29e518ce2c5604b6091cb3c
ddong8 <1413470138@qq.com>
2022-08-22 11:48:13 +0800

    update entry.

--- api_server.py
+++ api_server.py
@@ -31,4 +31,4 @@ async def sync_stock_data(date_str: str):
 
 
 if __name__ == '__main__':
-    uvicorn.run("api:app", host="0.0.0.0", port=9000)
+    uvicorn.run("api_server:app", host="0.0.0.0", port=9000)
